a586e3711875f20aa380091557b6dba220635f93
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-10-04 22:42:05 -0700

    html/css/js updates

--- script.js
+++ script.js
@@ -67,24 +67,26 @@ function searchRequest(event) {
     var weatherDay4 = data.daily[4]
     var weatherDay5 = data.daily[5]
 
+  
+
     console.log(data.daily[1])
 
     document.getElementById('todaysweather').innerHTML = `<h3>Today:</h3><ul><li>Temperature: ${weatherToday.temp}</li>
-    <li>Humidity: ${weatherToday.humidity}</li><li>UV Index: ${weatherToday.uvi}</li><li> Wind Speed: ${weatherToday.wind_speed}</li><li> Weather: ${weatherToday.weather[0].description}</li><li> Weather: ${weatherToday.weather[0].icon}</li></ul>`
+    <li>Humidity: ${weatherToday.humidity}</li><li>UV Index: ${weatherToday.uvi}</li><li> Wind Speed: ${weatherToday.wind_speed}</li><li> Weather: ${weatherToday.weather[0].description}</li></ul>`
 
     document.getElementById('Day 1').innerHTML = `<h4>5 Day Forecast:</h4><ul><li>Temperature: ${weatherDay1.temp}</li>
-    <li>Humidity: ${weatherDay1.humidity}</li><li>UV Index: ${weatherDay1.uvi}</li><li> Wind Speed: ${weatherDay1.wind_speed}</li><li> Weather: ${weatherDay1.weather[0].description}</li><li> Weather: ${weatherDay1.weather[0].icon}</li></ul>`
+    <li>Humidity: ${weatherDay1.humidity}</li><li>UV Index: ${weatherDay1.uvi}</li><li> Wind Speed: ${weatherDay1.wind_speed}</li><li> Weather: ${weatherDay1.weather[0].description}</li></ul>`
 
     document.getElementById('Day 2').innerHTML = `<ul><li>Temperature: ${weatherDay2.temp}</li>
-    <li>Humidity: ${weatherDay2.humidity}</li><li>UV Index: ${weatherDay2.uvi}</li><li> Wind Speed: ${weatherDay2.wind_speed}</li><li> Weather: ${weatherDay2.weather[0].description}</li><li> Weather: ${weatherDay2.weather[0].icon}</li></ul>`
+    <li>Humidity: ${weatherDay2.humidity}</li><li>UV Index: ${weatherDay2.uvi}</li><li> Wind Speed: ${weatherDay2.wind_speed}</li><li> Weather: ${weatherDay2.weather[0].description}</li></ul>`
 
     document.getElementById('Day 3').innerHTML = `<ul><li>Temperature: ${weatherDay3.temp}</li>
-    <li>Humidity: ${weatherDay3.humidity}</li><li>UV Index: ${weatherDay3.uvi}</li><li> Wind Speed: ${weatherDay3.wind_speed}</li><li> Weather: ${weatherDay3.weather[0].description}</li><li> Weather: ${weatherDay3.weather[0].icon}</li></ul>`
+    <li>Humidity: ${weatherDay3.humidity}</li><li>UV Index: ${weatherDay3.uvi}</li><li> Wind Speed: ${weatherDay3.wind_speed}</li><li> Weather: ${weatherDay3.weather[0].description}</li></ul>`
 
     document.getElementById('Day 4').innerHTML = `<ul><li>Temperature: ${weatherDay4.temp}</li>
-    <li>Humidity: ${weatherDay4.humidity}</li><li>UV Index: ${weatherDay4.uvi}</li><li> Wind Speed: ${weatherDay4.wind_speed}</li><li> Weather: ${weatherDay4.weather[0].description}</li><li> Weather: ${weatherDay4.weather[0].icon}</li></ul>`
+    <li>Humidity: ${weatherDay4.humidity}</li><li>UV Index: ${weatherDay4.uvi}</li><li> Wind Speed: ${weatherDay4.wind_speed}</li><li> Weather: ${weatherDay4.weather[0].description}</li></ul>`
     document.getElementById('Day 5').innerHTML = `<ul><li>Temperature: ${weatherDay5.temp}</li>
-    <li>Humidity: ${weatherDay5.humidity}</li><li>UV Index: ${weatherDay5.uvi}</li><li> Wind Speed: ${weatherDay5.wind_speed}</li><li> Weather: ${weatherDay5.weather[0].description}</li><li> Weather: ${weatherDay5.weather[0].icon}</li></ul>`
+    <li>Humidity: ${weatherDay5.humidity}</li><li>UV Index: ${weatherDay5.uvi}</li><li> Wind Speed: ${weatherDay5.wind_speed}</li><li> Weather: ${weatherDay5.weather[0].description}</li></ul>`
  
             })
         }
@@ -100,63 +102,6 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 });
 
-// create elements and append to hmtl
-
-// const api = {
-//     key: "f6591845cbace330d3376dc66277602d",
-//     base: "https://api.openweathermap.org/data/2.5/"
-// }
-
-// const searchbox = document.querySelector('.search-box');
-// searchbox.addEventListener('keypress', setQuery);
-
-// function setQuery(evt) {
-//     if (evt.keyCode == 13) {
-//         getResults(searchbox.value);
-//         console.log(searchbox.value);
-//     } 
-// }
-
-// function getResults (query) {
-//     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-//     .then(weather => {
-//         return weather.json();
-//     }).then(displayResults);
-// }
-
-// function displayResults (weather) {
-//     console.log(weather);
-//     let city = document.querySelector('.location .city');
-//     city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-//     let now = new Date();
-//     let date = document.querySelector('.location .date');
-//     date.innerText = dateBuilder(now);
-
-//     let temp = document.querySelector('.current .temp');
-//     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-//     let weatherEL = document.querySelector('.current .weather');
-//     weatherEL.innerText = weather.weather[0].main;
-
-//     let hilow = document.querySelector('.hi-low');
-//     hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`;
-// }
-
-// function dateBuilder (d) {
-//     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-//     let day = days[d.getDay()];
-//     let date = d.getDate();
-//     let month = months[d.getMonths()];
-//     let year = d.getFullYear();
-
-//     return `${day} ${date} ${month} ${year}`;
-
-// console.log('added', {cities} );
-// let pre = document.querySelector('#msg pre');
-// pre.textContent = '\n' + JSON.stringify(cities, '\t', 2);
 
 
 

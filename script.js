69bb1a447267ca6f47e74a8f3455bfb524b9abba
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-10-04 21:51:10 -0700

    html/css/js updates

--- script.js
+++ script.js
@@ -3,31 +3,45 @@ var inputEl = document.querySelector(".search-box");
 var buttonEl = document.querySelector(".search-btn");
 var selectEl = document.querySelector(".options");
 
-let cities = [];
+var cities = JSON.parse(localStorage.getItem('cities'));
+    if(JSON.parse(localStorage.getItem('cities')) !== null) {
+        var cities = JSON.parse(localStorage.getItem('cities')); 
+    } else {
+        var cities = [];
+    }
+    
 
 const addCity = (ev)=>{
-    ev.preventDefault();
+    // ev.preventDefault();
     let city = {
             id: Date.now(),
             city: document.getElementById('search-box').value
     }
     cities.push(city);
-    document.forms[0].reset(); 
-    
-    console.log('added', {cities} );
-    let pre = document.querySelector('#msg pre');
-    pre.textContent = '\n' + JSON.stringify(cities, '\t', 2);
-
-    localStorage.setItem('searchedCityList', JSON.stringify(cities) );
+    localStorage.setItem('cities', JSON.stringify(cities) );
+    // document.forms[0].reset(); 
 }
-document.addEventListener('DOMContentLoaded', ()=> {
-    document.getElementById('search-btn').addEventListener('click', searchRequest);
+
+$(document).ready(function () {
+    createCityButtons()
 });
+
+function createCityButtons() {
+        if (cities.length > 0) {
+            for (var i = 0; i < cities.length; i++) {
+                var btn = $("<button>")
+                btn.addClass("searchbtns");
+                btn.text(cities[i].city)
+                $('#searches').append(btn);
+                }
+        }
+    
+}
 
 function searchRequest(event) {
     event.preventDefault();
 
-    
+    addCity();
      var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputEl.value + "&appid=f6591845cbace330d3376dc66277602d";
 
     fetch(apiURL).then(function(response) {
@@ -53,10 +67,25 @@ function searchRequest(event) {
     var weatherDay4 = data.daily[4]
     var weatherDay5 = data.daily[5]
 
-    console.log(data.daily[1]);
+    console.log(data.daily[1])
 
-    weatherToday.insertAdjacentHTML('beforeend', '<p id=todaysweather></p>');
+    document.getElementById('todaysweather').innerHTML = `<h3>Today:</h3><ul><li>Temperature: ${weatherToday.temp}</li>
+    <li>Humidity: ${weatherToday.humidity}</li><li>UV Index: ${weatherToday.uvi}</li><li> Wind Speed: ${weatherToday.wind_speed}</li><li> Weather: ${weatherToday.weather[0].description}</li><li> Weather: ${weatherToday.weather[0].icon}</li></ul>`
 
+    document.getElementById('Day 1').innerHTML = `<h4>5 Day Forecast:</h4><ul><li>Temperature: ${weatherDay1.temp}</li>
+    <li>Humidity: ${weatherDay1.humidity}</li><li>UV Index: ${weatherDay1.uvi}</li><li> Wind Speed: ${weatherDay1.wind_speed}</li><li> Weather: ${weatherDay1.weather[0].description}</li><li> Weather: ${weatherDay1.weather[0].icon}</li></ul>`
+
+    document.getElementById('Day 2').innerHTML = `<ul><li>Temperature: ${weatherDay2.temp}</li>
+    <li>Humidity: ${weatherDay2.humidity}</li><li>UV Index: ${weatherDay2.uvi}</li><li> Wind Speed: ${weatherDay2.wind_speed}</li><li> Weather: ${weatherDay2.weather[0].description}</li><li> Weather: ${weatherDay2.weather[0].icon}</li></ul>`
+
+    document.getElementById('Day 3').innerHTML = `<ul><li>Temperature: ${weatherDay3.temp}</li>
+    <li>Humidity: ${weatherDay3.humidity}</li><li>UV Index: ${weatherDay3.uvi}</li><li> Wind Speed: ${weatherDay3.wind_speed}</li><li> Weather: ${weatherDay3.weather[0].description}</li><li> Weather: ${weatherDay3.weather[0].icon}</li></ul>`
+
+    document.getElementById('Day 4').innerHTML = `<ul><li>Temperature: ${weatherDay4.temp}</li>
+    <li>Humidity: ${weatherDay4.humidity}</li><li>UV Index: ${weatherDay4.uvi}</li><li> Wind Speed: ${weatherDay4.wind_speed}</li><li> Weather: ${weatherDay4.weather[0].description}</li><li> Weather: ${weatherDay4.weather[0].icon}</li></ul>`
+    document.getElementById('Day 5').innerHTML = `<ul><li>Temperature: ${weatherDay5.temp}</li>
+    <li>Humidity: ${weatherDay5.humidity}</li><li>UV Index: ${weatherDay5.uvi}</li><li> Wind Speed: ${weatherDay5.wind_speed}</li><li> Weather: ${weatherDay5.weather[0].description}</li><li> Weather: ${weatherDay5.weather[0].icon}</li></ul>`
+ 
             })
         }
         
@@ -64,7 +93,12 @@ function searchRequest(event) {
             })
         }
     });
+    createCityButtons();
 }
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('search-btn').addEventListener('click', searchRequest);
+
+});
 
 // create elements and append to hmtl
 
@@ -120,7 +154,9 @@ function searchRequest(event) {
 
 //     return `${day} ${date} ${month} ${year}`;
 
-
+// console.log('added', {cities} );
+// let pre = document.querySelector('#msg pre');
+// pre.textContent = '\n' + JSON.stringify(cities, '\t', 2);
 
 
 
